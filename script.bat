e098607beacae1faf2b27d604149787247f89323
karnampraveen <karnampraveen@gmail.com>
2024-06-10 13:46:09 +0100

    Testing pipeline 1

--- script.bat
+++ script.bat
@@ -1,4 +1,4 @@
 echo "Hello World!"
 echo "This is from Jenkins Build"
 echo "Now with automated pipeline to build whenever a push happens using a git Webhook"
-echo "Also added pipeline now"
+echo "Also added pipeline now - test 3"
